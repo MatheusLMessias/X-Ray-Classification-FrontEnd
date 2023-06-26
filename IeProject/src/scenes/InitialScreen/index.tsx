6c453ef61555efbc0f0a9d17279c39c28b585386
MatheusLMessias <matheusl.messias09@gmail.com>
2023-06-26 00:28:16 -0300

    Criando navegacao

---
+++ IeProject/src/scenes/InitialScreen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
+
+const InitialScreen = () => {
+    return (
+        <View>
+          <Text>asdasd</Text>
+        </View>
+      );
+
+}
+
+export default InitialScreen;

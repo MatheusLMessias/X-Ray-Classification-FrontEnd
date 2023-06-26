8591b4fa264e2dd8e9363e6606b4644ecd35076a
MatheusLMessias <matheusl.messias09@gmail.com>
2023-06-26 14:21:25 -0300

    Criando component 'buttons' da tela inicial

--- IeProject/src/scenes/InitialScreen/index.tsx
+++ IeProject/src/scenes/InitialScreen/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
+import MainButton from '../../components/buttons';
+import styles from './styles';
 
-const InitialScreen = () => {
+interface InitialScreenProps {
+  navigation: any
+}
+
+const InitialScreen = ({navigation}: InitialScreenProps) => {
     return (
-        <View>
-          <Text>asdasd</Text>
+        <View style={styles.containerButton}>
+          <MainButton
+            route={() => navigation.navigate("Login")}
+            text="Tirar Foto"
+          />
+          <MainButton
+            route={() => navigation.navigate("Login")}
+            text="Inserir Foto"
+          />
+          <MainButton
+            route={() => navigation.navigate("Login")}
+            text="Histórico de Imagens"
+          />
         </View>
       );
-
 }
 
 export default InitialScreen;
---
+++ IeProject/src/scenes/InitialScreen/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    containerButton: {
+        marginTop: 50,
+        alignItems: "center",
+        justifyContent: "center",
+      },
+});
+
+export default styles;

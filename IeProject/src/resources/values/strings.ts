8390c4941172461b8e27fc08f0dfd8605a2672d1
MatheusLMessias <matheusl.messias09@gmail.com>
2023-06-26 22:39:36 -0300

    Criando tela de histórico de imagens

--- IeProject/src/resources/values/strings.ts
+++ IeProject/src/resources/values/strings.ts
@@ -1,1 +1,3 @@
 export const TEXTO_BOAS_VINDAS_TELA_INICIAL = 'Boas vindas fulano, no aplicativo XXXX você pode tirar ou selecionar fotos dos seus raios-X, para que nossa IA te diagnostique.';
+
+export const HEADER_TELA_HISTORICO_IMAGENS = 'Histórico de Imagens'
---
+++ IeProject/src/scenes/InitialScreen/hooks/useInitialScreen.ts
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const UseInitialScreen = () => {
+
+    const [initial, setInitial] = useState<boolean>(true);
+    
+    return{
+        setInitial,
+        initial
+    }
+}
+
+export default UseInitialScreen;
